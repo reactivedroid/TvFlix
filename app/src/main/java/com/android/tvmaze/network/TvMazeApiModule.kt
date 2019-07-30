da96fc8a1bb3fc1e9e4206b723dbf392f8cbae53
Ashwini Kumar <kumar.ashwini009@gmail.com>
2019-07-30 15:42:08 +0530

    Remove rxjava adaptor

--- app/src/main/java/com/android/tvmaze/network/TvMazeApiModule.kt
+++ app/src/main/java/com/android/tvmaze/network/TvMazeApiModule.kt
@@ -4,7 +4,6 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Named
 import javax.inject.Singleton
@@ -21,7 +20,6 @@ class TvMazeApiModule {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(MoshiConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(okHttpClient)
             .build().create(TvMazeApi::class.java)
     }
